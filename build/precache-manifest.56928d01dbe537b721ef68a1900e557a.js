3cf7afbb336c72ac6b3c7ae016abd2f8479f5a3b
Abhishek kumar mishra <93533712+abhimis12@users.noreply.github.com>
2022-05-18 01:47:33 +0530

    Add files via upload

---
+++ build/precache-manifest.56928d01dbe537b721ef68a1900e557a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4097c51d68a21f27d6172f566018ffb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01d1b381baa6336d6140",
+    "url": "/static/css/main.fe5f61cd.chunk.css"
+  },
+  {
+    "revision": "b4506d3408ba5982b893",
+    "url": "/static/js/2.50d6022a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.50d6022a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01d1b381baa6336d6140",
+    "url": "/static/js/main.ff39dda9.chunk.js"
+  },
+  {
+    "revision": "f80ee22dea2d62d5bc8f",
+    "url": "/static/js/runtime-main.455cf462.js"
+  }
+]);
